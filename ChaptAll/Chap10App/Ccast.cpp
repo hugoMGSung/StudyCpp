83fc6a53f0e50de101e8fd97ac65ae6230cd0a79
hugoMGSung <personar95@naver.com>
2020-04-29 12:29:06 +0900

    오전 커밋

--- ChaptAll/Chap10App/Ccast.cpp
+++ ChaptAll/Chap10App/Ccast.cpp
@@ -8,13 +8,14 @@ int main() {
 	double d = 2147483647.99999; // 123.456;
 	int i;
 
-	double dd = numeric_limits<double>::max();
-	int di = numeric_limits<int>::max();
-	printf("%d\n\n", di);
+	//double dd = numeric_limits<double>::max();
+	//int di = numeric_limits<int>::max();
+	//printf("%d\n\n", di);
 	//printf("%f\n\n", dd);
-	i = static_cast<int>(d);
-	printf("%d\n", i);
+	//i = static_cast<int>(d);
+	//printf("%d\n", i);
 	//pi = static_cast<int*>(str);
 	pi = (int*)str;
 	//printf("%d %x\n", *pi, *pi);
+	printf("%x %x\n", *pi, *(pi + 1));
 }
